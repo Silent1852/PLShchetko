cb11d9e6bd5b6bd0a76484266d114bff552bdc9d
Silent1852 <marker.1337@yandex.ru>
2020-02-16 23:38:29 +0700

    Lab1
    Lab1

---
+++ Lab1/2/2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream> 
+#include <fstream> 
+#include <string> 
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int a, b,c, k, size;
+	cout << "Введите размер массива: ";
+	cin >> size;
+	int* arr = new int[size];
+	cout << "Введите интервал [a,b]:" << endl;
+	cout << "a= ";
+	cin >> a;
+	cout << "b= ";
+	cin >> b;
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] =a+rand() % (b-a+1);
+		cout << arr[i] << endl;
+	}
+	cout << "Введите значение k: ";
+	cin >> k;
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] < 0)
+		{
+			c = abs(arr[i]);
+			int p = 1;
+			while (c > 0)
+			{
+				p = p * 10;
+				c = abs(c) / 10;
+			}
+			arr[i] = -1*(k * p - arr[i]);
+		}
+		cout << arr[i] << endl;
+	}
+	delete[] arr;
+	return 0;
+}
---
+++ Lab1/Lab1/1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream> 
+#include <fstream> 
+#include <string> 
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int k, count = 0, size = 10;
+	int* arr = new int[size];
+	string path = "array.txt";
+	ifstream fin;
+	fin.open(path);
+	if (!fin.is_open())
+	{
+		cout << "Файл не открыт\n";
+		return 0;
+	}
+	cout << "Файл открыт\n";
+	string str;
+	while (!fin.eof())
+	{
+		str = "";
+		fin >> str;
+		count++;
+	}
+	size = count;
+	fin.clear();
+	fin.seekg(0, ios::beg);
+	for (int i = 0; i < size; i++)
+		fin >> arr[i];
+	cout << "Введите значение k: ";
+	cin >> k;
+	for (int i = 0; i < size; i++)
+		if (arr[i] > k)
+		{
+			cout << arr[i] << "\n";
+}
+	delete[] arr;
+	return 0;
+}
