b7661e9cd4b4960e4d1a43ce8fda40f1f84df300
Silent1852 <marker.1337@yandex.ru>
2020-04-07 17:29:11 +0700

    Lab2

---
+++ Lab2/Lab2-SPEC(4)/Lab2-SPEC(4).cpp
@@ -0,0 +1,262 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const char X = 'X';
+const char O = 'O';
+const char EMPTY = ' ';
+const char TIE = 'T';
+const char NO_ONE = 'N';
+
+void instructions();
+char askYesNo(string question);
+int askNumber(string question, int high, int low = 0);
+char humanPiece();
+char opponent(char piece);
+void displayBoard(const vector<char>* const pBoard);
+char winner(const vector<char>* const pBoard);
+bool isLegal(const vector<char>* const pBoard, int move);
+int humanMove(const vector<char>* const pBoard, char human);
+int computerMove(vector<char> board, char computer);
+void announceWinner(char winner, char computer, char human);
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int move;
+    const int NUM_SQUARES = 9;
+    vector<char> board(NUM_SQUARES, EMPTY);
+
+    instructions();
+    char human = humanPiece();
+    char computer = opponent(human);
+    char turn = X;
+    displayBoard(&board);
+
+    while (winner(&board) == NO_ONE)
+    {
+        if (turn == human)
+        {
+            move = humanMove(&board, human);
+            board[move] = human;
+        }
+        else
+        {
+            move = computerMove(board, computer);
+            board[move] = computer;
+        }
+        displayBoard(&board);
+        turn = opponent(turn);
+    }
+
+    announceWinner(winner(&board), computer, human);
+
+    return 0;
+}
+
+
+void instructions()
+{
+    cout << " *** Игра в крестики-нолики ***  \n\n";
+    cout << "Делать ход нужно согласно доске, введя число от 0 до 8: \n\n";
+
+    cout << "       0 | 1 | 2\n";
+    cout << "       ---------\n";
+    cout << "       3 | 4 | 5\n";
+    cout << "       ---------\n";
+    cout << "       6 | 7 | 8\n\n";
+}
+
+char askYesNo(string question)
+{
+    char response;
+    do
+    {
+        cout << question << " (y/n): ";
+        cin >> response;
+    } while (response != 'y' && response != 'n');
+
+    return response;
+}
+
+int askNumber(string question, int high, int low)
+{
+    int number;
+    do
+    {
+        cout << question << " (" << low << " - " << high << "): ";
+        cin >> number;
+    } while (number > high || number < low);
+
+    return number;
+}
+
+char humanPiece()
+{
+    char go_first = askYesNo(" Желаете делать первый ход?");
+    if (go_first == 'y')
+    {
+        return X;
+    }
+    else
+    {
+        return O;
+    }
+}
+
+char opponent(char piece)
+{
+    if (piece == X)
+    {
+        return O;
+    }
+    else
+    {
+        return X;
+    }
+}
+
+void displayBoard(const vector<char>* const pBoard)
+{
+    cout << "\n\t" << (*pBoard)[0] << " | " << (*pBoard)[1] << " | " << (*pBoard)[2];
+    cout << "\n\t" << "---------";
+    cout << "\n\t" << (*pBoard)[3] << " | " << (*pBoard)[4] << " | " << (*pBoard)[5];
+    cout << "\n\t" << "---------";
+    cout << "\n\t" << (*pBoard)[6] << " | " << (*pBoard)[7] << " | " << (*pBoard)[8];
+    cout << "\n\n";
+}
+
+char winner(const vector<char>* const pBoard)
+{
+    // все выигрышные позиции
+    const int WINNING_ROWS[8][3] = { { 0, 1, 2 },
+    { 3, 4, 5 },
+    { 6, 7, 8 },
+    { 0, 3, 6 },
+    { 1, 4, 7 },
+    { 2, 5, 8 },
+    { 0, 4, 8 },
+    { 2, 4, 6 } };
+    const int TOTAL_ROWS = 8;
+
+    // если есть три одинаково заполненных выигрышных позиции, то есть победитель
+    for (int row = 0; row < TOTAL_ROWS; ++row)
+    {
+        if (((*pBoard)[WINNING_ROWS[row][0]] != EMPTY) &&
+            ((*pBoard)[WINNING_ROWS[row][0]] == (*pBoard)[WINNING_ROWS[row][1]]) &&
+            ((*pBoard)[WINNING_ROWS[row][1]] == (*pBoard)[WINNING_ROWS[row][2]]))
+        {
+            return (*pBoard)[WINNING_ROWS[row][0]];
+        }
+    }
+
+    // проверка на ничью, если он путсых квадратов
+    if (count(pBoard->begin(), pBoard->end(), EMPTY) == 0)
+        return TIE;
+
+    return NO_ONE;
+}
+
+inline bool isLegal(int move, const vector<char>* pBoard)
+{
+    return ((*pBoard)[move] == EMPTY);
+}
+
+int humanMove(const vector<char>* const pBoard, char human)
+{   
+    cout << " *** Ваш ход *** \n";
+    int move = askNumber(" Куда будете ходить?", (pBoard->size() - 1));
+    while (!isLegal(move, pBoard))
+    {
+        cout << "\nЭта позиция занята.\n";
+        move = askNumber("\nКуда будете ходить?", (pBoard->size() - 1));
+    }
+    return move;
+}
+
+int computerMove(vector<char> board, char computer)
+{
+    unsigned int move = 0;
+    bool found = false;
+    cout << " *** Ход компьютера ***\n";
+    //если компьютер может выиграть на след. ходу
+    while (!found && move < board.size())
+    {
+        if (isLegal(move, &board))
+        {
+            //попытка хода
+            board[move] = computer;
+            //проверка на возможный выигрышь
+            found = winner(&board) == computer;
+            //отмена хода
+            board[move] = EMPTY;
+        }
+        if (!found)
+        {
+            ++move;
+        }
+    }
+
+    //если игрок может выиграть на след. ходу,то компьютер пытается помешать
+    if (!found)
+    {
+        move = 0;
+        char human = opponent(computer);
+
+        while (!found && move < board.size())
+        {
+            if (isLegal(move, &board))
+            {
+                //попытка хода
+                board[move] = human;
+                //проверка на возможный выигрышь
+                found = winner(&board) == human;
+                //отмена хода
+                board[move] = EMPTY;
+            }
+            if (!found)
+            {
+                ++move;
+            }
+        }
+    }
+
+    // выбор лучшего открытого квадрата для компьютера
+    if (!found)
+    {
+        move = 0;
+        unsigned int i = 0;
+
+        const int BEST_MOVES[] = { 4, 0, 2, 6, 8, 1, 3, 5, 7 };
+        while (!found && i < board.size())
+        {
+            move = BEST_MOVES[i];
+            if (isLegal(move, &board))
+            {
+                found = true;
+            }
+            ++i;
+        }
+    }
+    return move;
+}
+
+void announceWinner(char winner, char computer, char human)
+{
+    if (winner == computer)
+    {
+        cout <<" *** Компьютер выиграл! ***\n";
+
+    }
+    else if (winner == human)
+    {
+        cout <<" *** Вы выиграли! ***\n";
+    }
+    else
+    {
+        cout << " *** Ничья ***\n";
+    }
+}
---
+++ Lab2/Lab2/Lab2-3.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+using namespace std;
+void menu();
+void results(int** n, int* n1, int a, int b);
+void winners(int* n, int a);
+int main()
+{   
+    setlocale(LC_ALL, "");
+    int n, m, key = 1;
+    menu();
+    cout << endl; cin >> key;
+    srand(time(NULL));
+    do
+        switch (key)
+        {
+        case 1:
+            n = rand() % 31;
+            m = rand() % 11;
+            break;
+        case 2:
+            cout << "Введите количество стрелков (n) и количество выстрелов (m)\n";
+            cin >> n;
+            cin >> m;
+            break;
+        case 0:
+            break;
+        default:
+            cout << "ERROR!\n";
+            break;
+        }
+    while (n == 0 || m == 0 || n == 1 || m == 1);
+    cout << "Кол-во стрелков:" << n << "\nКол-во выстрелов:" << m << "\n";
+    int** a;
+    a = new int* [n];
+    for (int i = 0; i < n; i++)
+        a[i] = new int[m];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            a[i][j] = rand() % 11;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%5d", a[i][j]);
+        }
+        cout << endl;
+    }
+    int* res;
+    res = new int[n];
+    results(a, res, n, m);
+    winners(res, n);
+    return 0;
+}
+
+void menu()
+{
+    cout << "Выберите режим работы:\n";
+    cout << endl;
+    cout << "1 - Случайные данные\n";
+    cout << "2 - Ручной ввод данных\n";
+    cout << "0 - Выход\n";
+}
+
+void results(int** n, int* n1, int a, int b)
+{
+    int s;
+    for (int i = 0; i < a; i++)
+    {
+        s = 0;
+        for (int j = 0; j < b; j++)
+            s += n[i][j];
+        n1[i] = s;
+    }
+    cout << "\nРезультаты: ";
+    for (int i = 0; i < a; i++)
+        cout << n1[i] << " ";
+}
+
+void winners(int* n, int a)
+{
+    int max;
+    max = n[0];
+    for (int i = 0; i < a; i++)
+    {
+        if (n[i] > max)
+            max = n[i];
+    }
+    cout << "\n" << max << "\nПобедители:\n";
+    for (int i = 0; i < a; i++)
+    {
+        if (n[i] == max)
+        {
+            cout << "Номер стрелка " << i + 1 << endl;
+        }
+    }
+}
